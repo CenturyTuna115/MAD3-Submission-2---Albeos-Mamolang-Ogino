fd60ab5a6dee1565de5958646f1c5f0432a72886
enjaayyy <enujeemontesaarubeosu@gmail.com>
2024-06-24 15:33:08 +0800

    numbers 1,2,3,4,5

--- mobile-3-session-1-stateful-management-REST-consumption/android/app/src/main/kotlin/com/example/state_change_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.state_change_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ mobile-3-session-1-stateful-management-REST-consumption/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Mobile\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\MobileProject\MAD3-Submission-2---Albeos-Mamolang-Ogino-1_2_3\mobile-3-session-1-stateful-management-REST-consumption\MAD3-Submission-2---Albeos-Mamolang-Ogino\mobile-3-session-1-stateful-management-REST-consumption"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
